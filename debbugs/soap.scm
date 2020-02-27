gnu-mirror-unofficial/guile-debbugs

commit 6835883940f67b9fe34b0a7d47c52cf8a9b27407

debbugs: soap-invoke: Stream response.
* debbugs/soap.scm (soap-invoke): Stream the POST response.

debbugs/soap.scm
@@ -98,15 +98,16 @@ response body."
          (req-xml (call-with-output-string
                    (lambda (port)
                      (sxml->xml (soap-request-body request) port)))))
-    (receive (response body)
+    (receive (response body-port)
         (http-post uri
                    #:body req-xml
                    #:headers
                    `((content-type . (text/xml))
                      (content-length . ,(string-length req-xml)))
+                   #:streaming? #t
                    #:decode-body? #t)
       ((soap-request-callback request)
-       (xml->sxml body #:trim-whitespace? #t)))))
+       (xml->sxml body-port #:trim-whitespace? #t)))))
 
 (define (soap-invoke* . args)
   "Cache the return value of SOAP-INVOKE.  Return the cached value if

commit f7f2f759a6902f6ce54a13cb1f28e6161c431e05

soap: Close body port.
This fixes a regression introduced by commit
6835883940f67b9fe34b0a7d47c52cf8a9b27407.

* debbugs/soap.scm (soap-invoke): Do not leak the body port.

debbugs/soap.scm
@@ -154,7 +154,13 @@ the SXML response body."
                    #:streaming? #t
                    #:decode-body? #t)
       ((soap-request-callback request)
-       (xml->sxml body-port #:trim-whitespace? #t)))))
+       (dynamic-wind
+         (const #f)
+         (lambda ()
+           (xml->sxml body-port #:trim-whitespace? #t))
+         (lambda ()
+           (unless (port-closed? body-port)
+             (close-port body-port))))))))
 
 (define (soap-invoke* . args)
   "Cache the return value of SOAP-INVOKE.  Return the cached value if